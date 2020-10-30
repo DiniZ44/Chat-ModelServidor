47fc7d4231ac80becc641d9f7777a507ea14a912
Hakkinen Diniz <santoshakkinen7@gmail.com>
2020-10-30 13:41:35 -0200

    First Commit

---
+++ src/com/chat/app/Servidor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chat.app;
+import com.chat.app.frame.ServidorFrame;
+import com.chat.app.service.ServidorService;
+
+/**
+ *
+ * @author santo
+ */
+public class Servidor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new ServidorService();
+    }
+}
